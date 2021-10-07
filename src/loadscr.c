944c38361748566751df27cb2949e833d7ce247b
IgorSou3000 <IgorSDS2005@gmail.com>
2021-10-07 11:29:28 -0300

    Improved loading screen

--- src/loadscr.c
+++ src/loadscr.c
@@ -21,7 +21,7 @@ void LoadScr_Start(void)
 	
 	//Load loading screen texture
 	RECT loading_src = {0, 0, 255, 255};
-	RECT loading_dst = {(SCREEN_WIDTH - 300) >> 1, (SCREEN_HEIGHT - 224) >> 1, 300, 224};
+	RECT loading_dst = {(SCREEN_WIDTH - 320) >> 1, (SCREEN_HEIGHT - 224) >> 1, 327, 23};
 	Gfx_Tex loading_tex;
 	Gfx_SetClear(0,0, 8);
 	Gfx_LoadTex(&loading_tex, IO_Read("\\MENU\\LOADING.TIM;1"), GFX_LOADTEX_FREE);
